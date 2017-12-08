4f27501720b850259b3bad547c24401eea971526
Gotti <mgotting@hm.edu>
2017-12-08 11:42:54 +0100

    ok

--- javascript/check.js
+++ javascript/check.js
@@ -1,8 +1,14 @@
 function ageChecker(age) {
-    if (age.value > 17) {
+    var exist_e = document.getElementsByClassName("Ü18");
+    if (age.value > 17 && exist_e.length<1) {
         var new_p = document.createElement("p");
+        new_p.className="Ü18";
         var new_t = document.createTextNode("30.06.2018 Hüttenausflug");
         new_p.appendChild(new_t);
         document.getElementById("events").appendChild(new_p);
+    } else if (age.value < 18 && exist_e.length>0){
+        for(var i=0; i<exist_e.length;i++){
+            exist_e[i].remove();
+        }
     }
 }
